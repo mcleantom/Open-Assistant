e30ca041cedd77cd037fda6f6cf7311ea06e8ff1
Tom McLean <64887735+mcleantom@users.noreply.github.com>
2023-02-15 21:47:40 +0000

    Removes wiki links from the comments and parses the comments into a list of comments

--- openassistant/datasets/wikipedia_talk_pages/prepare.py
+++ openassistant/datasets/wikipedia_talk_pages/prepare.py
@@ -8,7 +8,6 @@ from pathlib import Path
 import re
 import requests
 from bs4 import BeautifulSoup
-import tempfile
 import urllib.request
 from lxml import etree as et
 from bz2 import BZ2File
@@ -27,12 +26,53 @@ class ConversationTree(BaseModel):
     metadata: Dict[str, Any]
 
 
+def split_comments(comment_text: str) -> list[str]:
+    new_comment_regex_split = re.compile("(\n:+)")
+    comments = new_comment_regex_split.split(comment_text)
+    joined_comments = [comments[0]]
+    for split, comment in zip(comments[1::2], comments[2::2]):
+        assert (new_comment_regex_split.match(split))
+        joined_comments.append(split + comment)
+    return joined_comments
+
+
+def f(L):
+    pass
+
+
+def remove_wiki_links(comment: str) -> str:
+    # Replaces instances of [[foo]] with foo unless it has an | so [[foo|bar]] is kept the same.
+    return re.sub(r'\[\[([^|]*)?\]\]', r'\1', comment)
+
+
+def clean_comment(comment: str) -> str:
+    """
+    We keep the indentation level (i.e. the ::: at the start of the string) however we clean all the weird wiki-text
+    formatting.
+    """
+    comment = remove_wiki_links(comment)
+    return comment
+
+
+def parse_comment_text(comment_text: str) -> list[str]:
+    comments = split_comments(comment_text)
+    comments = [clean_comment(comment) for comment in comments]
+    # print("\n".join(comments))
+    return comments
+
+
+def build_tree(comments) -> ConversationTree:
+    pass
+
+
 def parse_talk_page(element: lxml.etree._Element, child: lxml.etree._Element):
     text = element.getchildren()[-1].getchildren()[7].text
     topics = re.split("(==.*==)", text)[1:]
-    for topic, comments in zip(topics[0::2], topics[1::2]):
+    for topic, comments_text in zip(topics[0::2], topics[1::2]):
         assert(topic.startswith("=="))
         assert(topic.endswith("=="))
+        comments = parse_comment_text(comments_text)
+        tree = build_tree(comments)
 
 
 def parse_page(element: lxml.etree._Element):
